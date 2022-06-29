5205300de78d498427b1482a7255fb4287b89fab
OliveiraDCI <oliveira.sein.dci@gmail.com>
2022-06-29 13:20:26 +0200

    Initial commit - Exercise concluded.

--- solution.js
+++ solution.js
@@ -1,1 +1,35 @@
+// 1
+const str = "Practicing Javascript";
+console.log(str);
 
+// 2
+console.log(str[1], str[5]);
+
+// 3
+const firstName = "Rafael";
+
+// 4
+const age = 36;
+
+// 5
+const year = 2022;
+
+// 6
+console.log(firstName + " is " + age + " in " + year);
+
+// 7
+const isMarried = true;
+
+// 8
+console.log(firstName + " is married: " + isMarried);
+
+// 9
+const programmingLanguage = "JavaScript";
+const isFun = true;
+console.log(programmingLanguage + " is fun: " + isFun);
+
+// 10
+console.log(programmingLanguage[0], programmingLanguage[4]);
+
+// 11
+console.log(programmingLanguage.length);
